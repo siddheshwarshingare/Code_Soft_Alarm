72ce9d238a763bfec56651fc2101e31492e7102a
siddheshwarshingare <144676414+siddheshwarshingare@users.noreply.github.com>
2024-08-23 11:07:12 +0530

    This is alaram project

---
+++ lib/ring.dart
@@ -0,0 +1,74 @@
+import 'package:alarm/alarm.dart';
+import 'package:flutter/material.dart';
+import 'package:simple_ripple_animation/simple_ripple_animation.dart';
+
+class ExampleAlarmRingScreen extends StatelessWidget {
+  final AlarmSettings alarmSettings;
+
+  const ExampleAlarmRingScreen({Key? key, required this.alarmSettings})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "You alarm is ringing...",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const RippleAnimation(
+              color: Colors.black,
+              delay: Duration(milliseconds: 300),
+              repeat: true,
+              minRadius: 75,
+              ripplesCount: 6,
+              duration: Duration(milliseconds: 6 * 300),
+              child: Text("🔔", style: TextStyle(fontSize: 50)),
+            ),
+            // const Text("🔔", style: TextStyle(fontSize: 50)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RawMaterialButton(
+                  onPressed: () {
+                    final now = DateTime.now();
+                    Alarm.set(
+                      alarmSettings: alarmSettings.copyWith(
+                        dateTime: DateTime(
+                          now.year,
+                          now.month,
+                          now.day,
+                          now.hour,
+                          now.minute,
+                          0,
+                          0,
+                        ).add(const Duration(minutes: 1)),
+                      ),
+                    ).then((_) => Navigator.pop(context));
+                  },
+                  child: Text(
+                    "Snooze",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                ),
+                RawMaterialButton(
+                  onPressed: () {
+                    Alarm.stop(alarmSettings.id)
+                        .then((_) => Navigator.pop(context));
+                  },
+                  child: Text(
+                    "Stop",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
